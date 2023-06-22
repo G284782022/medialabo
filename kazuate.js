c95d75808d8e8e49a9bf6e2c7c1a91c2ca388b78
G284782022 <228478@st.takushoku-u.ac.jp>
2023-06-22 23:16:32 +0900

    数当てゲーム

--- kazuate.js
+++ kazuate.js
@@ -18,7 +18,7 @@ function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
   kaisu = kaisu + 1;
-  console.log(kaisu+"回目")
+  console.log(kaisu+"回目の予想: "+yoso);
   if (kaisu > 3){
     console.log("答えは"+kotae+"でした．すでにゲームは終わっています")
  }else if(kaisu === 3){
